5f0c041303c37cdd64b884cf6ede0a9c70fe6037
subashchandar021 <82366513+subashchandar021@users.noreply.github.com>
2021-04-20 11:59:29 +0530

    Add files via upload

---
+++ cat1.js
@@ -0,0 +1,19 @@
+var cat = {
+    "name": 'Fluffy',
+    "activities":['play', 'eat cat food'],
+    "catFriends":[
+                  {
+                   "name": 'bar',
+                   "activities": ['be grumpy', 'eat bread omlete'],
+                   "weight": 8,
+                  "furcolor": 'white'
+                  }, 
+                 {
+                   "name": 'foo',
+                    "activities": ["sleep", "pre-sleep naps"],
+                    "weight": 3
+                  }
+                ]
+               }
+               cat.name="Fluffyy";
+               console.log(cat.name);
